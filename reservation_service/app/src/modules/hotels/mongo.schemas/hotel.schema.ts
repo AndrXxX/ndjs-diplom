4c04f407ee4dfc872f6dff2f4c211501bf7ed43f
Andrew Garetov <andrew1gar@yandex.ru>
2023-09-09 13:27:28 +0300

    #1 Добавлена схема HotelSchema

---
+++ reservation_service/app/src/modules/hotels/mongo.schemas/hotel.schema.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+import { iHotel } from "src/interfaces/hotel.interface";
+
+export type HotelDocument = HydratedDocument<Hotel>;
+
+@Schema()
+export class Hotel implements iHotel {
+  @Prop( { required: [true, 'Не указано название'] })
+  title: string;
+
+  @Prop( { required: false })
+  description: string;
+
+  @Prop( { required: [true, 'Не указана дата добавления'] })
+  createdAt: Date;
+
+  @Prop( { required: [true, 'Не указана дата обновления'] })
+  updatedAt: Date;
+}
+
+export const HotelSchema = SchemaFactory.createForClass(Hotel);

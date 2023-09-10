34783184a090ca1958833f724adaec7de3dc37df
Andrew Garetov <andrew1gar@yandex.ru>
2023-09-10 22:42:04 +0300

    #1 Перенос CreateUserDto в подкаталог dto

--- reservation_service/app/src/modules/users/users.service.ts
+++ reservation_service/app/src/modules/users/users.service.ts
@@ -3,8 +3,8 @@ import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { GenerateHashService } from "src/modules/auth/generate-hash.service";
 import { ID } from "src/types/ID";
+import { CreateUserDto } from "./dto/user-create.dto";
 import { SearchUserParams } from "./interfaces/search-user-params.interface";
-import { CreateUserDto } from "./interfaces/user-create.interface";
 import { IUserService } from "./interfaces/user-service.interface";
 import { User, UserDocument } from "./mongo.schemas/user.schema";
 

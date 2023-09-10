7ecd26fb4b8939a783ff5e0901b844f1314ba64c
Andrew Garetov <andrew1gar@yandex.ru>
2023-09-10 22:52:38 +0300

    #1 Добавлен HotelsRoomService

---
+++ reservation_service/app/src/modules/hotels/hotels-room.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { ID } from "src/types/ID";
+import { CreateHotelRoomDto } from "./dto/create-hotel-room.dto";
+import { UpdateHotelRoomDto } from "./dto/update-hotel-room.dto";
+import { IHotelsRoomService } from "./interfaces/hotels-room-service.interface";
+import { SearchRoomsParams } from "./interfaces/search-rooms-params.interface";
+import { HotelRoom, HotelRoomDocument } from "./mongo.schemas/hotel-room.schema";
+
+@Injectable()
+export class HotelsRoomService implements IHotelsRoomService {
+    constructor(
+      @InjectModel(HotelRoom.name) private HotelRoomModel: Model<HotelRoomDocument>,
+    ) {}
+
+    public async create(data: Partial<CreateHotelRoomDto>): Promise<HotelRoomDocument> {
+        const model = new this.HotelRoomModel(data);
+        await model.save();
+        return model;
+    }
+
+    async findById(id: ID): Promise<HotelRoomDocument | undefined> {
+        return await this.HotelRoomModel.findById(id).select('-__v').exec();
+    }
+
+    async search(params: SearchRoomsParams): Promise<HotelRoomDocument[]> {
+        if (params.isEnabled === undefined) {
+            delete params.isEnabled;
+        }
+        return await this.HotelRoomModel.find(params).select('-__v').exec();
+    }
+
+    async update(id: ID, data: Partial<UpdateHotelRoomDto>): Promise<HotelRoomDocument | undefined> {
+        this.HotelRoomModel.updateOne({ id }, data);
+        return this.findById(id);
+    }
+}
--- reservation_service/app/src/modules/hotels/hotels.module.ts
+++ reservation_service/app/src/modules/hotels/hotels.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from "@nestjs/mongoose";
+import { HotelsRoomService } from "./hotels-room.service";
 import { HotelsService } from "./hotels.service";
 import { HotelRoom, HotelRoomSchema } from "./mongo.schemas/hotel-room.schema";
 import { Hotel, HotelSchema } from "./mongo.schemas/hotel.schema";
@@ -11,7 +12,7 @@ import { Hotel, HotelSchema } from "./mongo.schemas/hotel.schema";
       { name: HotelRoom.name, schema: HotelRoomSchema },
     ]),
   ],
-  providers: [HotelsService],
-  exports: [HotelsService],
+  providers: [HotelsService, HotelsRoomService],
+  exports: [HotelsService, HotelsRoomService],
 })
-export class UsersModule {}
+export class HotelsModule {}

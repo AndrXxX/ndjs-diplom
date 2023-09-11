bd487f93c39ce6c67cc3b5ac9cbf6e6f92dee1fb
Andrew Garetov <andrew1gar@yandex.ru>
2023-09-11 22:22:42 +0300

    #1 Добавлен SupportRequestEmployeeService

--- reservation_service/app/src/modules/support-request/support-request-employee.service.ts
+++ reservation_service/app/src/modules/support-request/support-request-employee.service.ts
@@ -2,14 +2,13 @@ import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { ID } from "src/types/ID";
-import { CreateSupportRequestDto } from "./interfaces/create-support-request-dto.interface";
 import { MarkMessagesAsReadDto } from "./interfaces/mark-messages-as-read-dto.interface";
-import { ISupportRequestClientService } from "./interfaces/support-request-client-service.interface";
+import { ISupportRequestEmployeeService } from "./interfaces/support-request-employee-service.interface";
 import { Message } from "./mongo.schemas/message.schema";
 import { SupportRequest, SupportRequestDocument } from "./mongo.schemas/support-request.schema";
 
 @Injectable()
-export class SupportRequestClientService implements ISupportRequestClientService {
+export class SupportRequestEmployeeService implements ISupportRequestEmployeeService {
 
     // TODO: Оповещения должны быть реализованы через механизм EventEmitter.
 
@@ -17,18 +16,9 @@ export class SupportRequestClientService implements ISupportRequestClientService
       @InjectModel(SupportRequest.name) private SupportRequestModel: Model<SupportRequestDocument>,
     ) {}
 
-    public async createSupportRequest(data: CreateSupportRequestDto): Promise<SupportRequest> {
-        const model = new this.SupportRequestModel(data);
-        model.createdAt = new Date();
-        model.isActive = true;
-        // model.messages.push() // TODO add message
-        await model.save();
-        return model;
-    }
-
-    public async markMessagesAsRead(params: MarkMessagesAsReadDto): Promise<void> {
+    public async markMessagesAsRead(params: MarkMessagesAsReadDto) {
         (await this.findById(params.supportRequest))?.messages
-          .filter(message => message.author != params.user)
+          .filter(message => message.author == params.user)
           .filter(message => message.sentAt < params.createdBefore)
           .forEach(message => {
               message.readAt = new Date();
@@ -37,11 +27,19 @@ export class SupportRequestClientService implements ISupportRequestClientService
     }
 
     public async getUnreadCount(supportRequest: ID): Promise<Message[]> {
-        // TODO: должен возвращать количество сообщений, которые были отправлены любым сотрудником поддержки и не отмечены прочитанным.
+        // TODO: должен возвращать количество сообщений, которые были отправлены пользователем и не отмечены прочитанными.
         // TODO: Возможно проверять роль
         return (await this.findById(supportRequest))
           ?.messages
           .filter(message => !message.readAt) || [];
+    }
+
+    public async closeRequest(supportRequest: ID): Promise<void> {
+        const request = await this.findById(supportRequest);
+        if (request) {
+            request.isActive = false;
+            await request.save();
+        }
     }
 
     private async findById(id: ID): Promise<SupportRequestDocument | undefined> {

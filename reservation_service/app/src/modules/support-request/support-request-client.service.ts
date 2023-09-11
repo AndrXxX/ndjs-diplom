d9d82f00e3d10a2d1db86d502abce9280ef572a3
Andrew Garetov <andrew1gar@yandex.ru>
2023-09-11 22:13:31 +0300

    #1 Добавлен SupportRequestClientService

---
+++ reservation_service/app/src/modules/support-request/support-request-client.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { ID } from "src/types/ID";
+import { CreateSupportRequestDto } from "./interfaces/create-support-request-dto.interface";
+import { MarkMessagesAsReadDto } from "./interfaces/mark-messages-as-read-dto.interface";
+import { ISupportRequestClientService } from "./interfaces/support-request-client-service.interface";
+import { Message } from "./mongo.schemas/message.schema";
+import { SupportRequest, SupportRequestDocument } from "./mongo.schemas/support-request.schema";
+
+@Injectable()
+export class SupportRequestClientService implements ISupportRequestClientService {
+
+    constructor(
+      @InjectModel(SupportRequest.name) private SupportRequestModel: Model<SupportRequestDocument>,
+    ) {}
+
+    public async createSupportRequest(data: CreateSupportRequestDto): Promise<SupportRequest> {
+        const model = new this.SupportRequestModel(data);
+        model.createdAt = new Date();
+        await model.save();
+        return model;
+    }
+
+    public async markMessagesAsRead(params: MarkMessagesAsReadDto): Promise<void> {
+        (await this.findById(params.supportRequest))?.messages
+          .filter(message => message.author != params.user)
+          .filter(message => message.sentAt < params.createdBefore)
+          .forEach(message => {
+              message.readAt = new Date();
+              message.save();
+          })
+    }
+
+    public async getUnreadCount(supportRequest: ID): Promise<Message[]> {
+        return (await this.findById(supportRequest))
+          ?.messages
+          .filter(message => !message.readAt) || [];
+    }
+
+    private async findById(id: ID): Promise<SupportRequestDocument | undefined> {
+        return await this.SupportRequestModel.findById(id).select('-__v').exec();
+    }
+}

23a6eeb9c7b7c235c13ffb0a1534a4c2dfe48c13
Andrew Garetov <andrew1gar@yandex.ru>
2023-09-23 15:24:41 +0300

    #1 Добавлен endpoint /auth/login

--- reservation_service/app/src/modules/users/users.controller.ts
+++ reservation_service/app/src/modules/users/users.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { DtoValidationPipe } from "src/validators/dto.validation.pipe";
 import { AuthService } from "../auth/auth.service";
+import { LocalAuthGuard } from "../auth/guards/local.auth.guard";
 import { CreateUserDto } from "./dto/user-create.dto";
+import { SigninUserDto } from "./interfaces/user-signin.interface";
 import { UsersService } from "./users.service";
 
 @Controller('api')
@@ -12,8 +14,24 @@ export class UsersController {
   ) {}
 
   @Post("/client/register")
-  async signup(@Body(DtoValidationPipe) createUserDto: CreateUserDto) {
+  async clientRegister(@Body(DtoValidationPipe) createUserDto: CreateUserDto) {
     createUserDto.role = 'client';
     await this.usersService.create(createUserDto);
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post("/auth/login")
+  async authLogin(
+    @Body(DtoValidationPipe) signinUserDto: SigninUserDto,
+    @Request() req: any,
+  ) {
+    // TODO: Доступно только не аутентифицированным пользователям.
+    // TODO: 401 - если пользователя с указанным email не существует или пароль неверный.
+    // TODO: Стартует сессию пользователя и выставляет Cookies.
+    return {
+      email: req.user.email,
+      name: req.user.name,
+      contactPhone: req.user.contactPhone,
+    }
   }
 }

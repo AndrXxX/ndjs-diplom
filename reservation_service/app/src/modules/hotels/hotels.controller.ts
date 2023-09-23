8d564f97f30d7c494a62008401bf6329b1e9a8c5
Andrew Garetov <andrew1gar@yandex.ru>
2023-09-23 14:33:24 +0300

    #1 Добавлен Endpoint /common/hotel-rooms/

--- reservation_service/app/src/modules/hotels/hotels.controller.ts
+++ reservation_service/app/src/modules/hotels/hotels.controller.ts
@@ -1,14 +1,19 @@
 import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { IHotelsRoomService } from "src/modules/hotels/interfaces/hotels-room-service.interface";
 import { CreateHotelDto } from "./dto/create-hotel.dto";
+import { FormatHotelRoomService } from "./format-hotel-room.service";
 import { FormatHotelService } from "./format-hotel.service";
 import { IHotelsService } from "./interfaces/hotels-service.interface";
 import { SearchHotelParams } from "./interfaces/search-hotel-params.interface";
+import { SearchRoomsParams } from "./interfaces/search-rooms-params.interface";
 
 @Controller('api')
 export class HotelsController {
   constructor(
     private hotelsService: IHotelsService,
+    private hotelRoomsService: IHotelsRoomService,
     private formatHotelService: FormatHotelService,
+    private formatHotelRoomService: FormatHotelRoomService,
   ) {}
 
   @Post('/admin/hotels/')
@@ -30,5 +35,12 @@ export class HotelsController {
     // 403 - если роль пользователя не admin.
     const hotels = await this.hotelsService.search(query);
     return hotels.map(hotel => this.formatHotelService.format(hotel));
+  }
+
+  @Get('/common/hotel-rooms/')
+  async hotelRoomsList(@Query() query: SearchRoomsParams) {
+    // TODO: Если пользователь не аутентифицирован или его роль client, то при поиске всегда должен использоваться флаг isEnabled: true.
+    const rooms = await this.hotelRoomsService.search(query);
+    return rooms.map(room => this.formatHotelRoomService.format(room));
   }
 }

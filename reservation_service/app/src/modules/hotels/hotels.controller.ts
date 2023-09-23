4ac45c829981b3bc1d2f147f295031b57aa57f86
Andrew Garetov <andrew1gar@yandex.ru>
2023-09-23 14:23:00 +0300

    #1 Использование formatHotelService

--- reservation_service/app/src/modules/hotels/hotels.controller.ts
+++ reservation_service/app/src/modules/hotels/hotels.controller.ts
@@ -1,15 +1,22 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { CreateHotelDto } from "src/modules/hotels/dto/create-hotel.dto";
-import { IHotelsService } from "src/modules/hotels/interfaces/hotels-service.interface";
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { CreateHotelDto } from "./dto/create-hotel.dto";
+import { FormatHotelService } from "./format-hotel.service";
+import { IHotelsService } from "./interfaces/hotels-service.interface";
 
 @Controller('api')
 export class HotelsController {
-  constructor(private hotelsService: IHotelsService) {
-  }
+  constructor(
+    private hotelsService: IHotelsService,
+    private formatHotelService: FormatHotelService,
+  ) {}
 
   @Post('/admin/hotels/')
   async addHotel(@Body() createHotelDto: CreateHotelDto) {
-    const { id, title, description } = await this.hotelsService.create(createHotelDto);
-    return { id, title, description }
+    // TODO: Доступно только аутентифицированным пользователям с ролью admin.
+    //
+    // Ошибки
+    // 401 - если пользователь не аутентифицирован;
+    // 403 - если роль пользователя не admin.
+    return this.formatHotelService.format(await this.hotelsService.create(createHotelDto))
   }
 }

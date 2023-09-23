d188f08c2781561c19c5ce86a449ae8695a42b83
Andrew Garetov <andrew1gar@yandex.ru>
2023-09-23 15:18:24 +0300

    #1 Подключение passport.session

--- reservation_service/app/src/main.ts
+++ reservation_service/app/src/main.ts
@@ -1,4 +1,6 @@
 import { NestFactory } from '@nestjs/core';
+import * as session from "express-session"
+import * as passport from "passport"
 import { AppModule } from './app.module';
 import config from './config';
 import { UnifiedExceptionFilter } from "./filters/unified.exception.filter";
@@ -6,6 +8,17 @@ import { UnifiedExceptionFilter } from "./filters/unified.exception.filter";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new UnifiedExceptionFilter());
+
+  app.use(
+    session({
+      secret: config.cookieSecret,
+      resave: false,
+      saveUninitialized: false,
+    })
+  )
+  app.use(passport.initialize())
+  app.use(passport.session())
+
   await app.listen(config.port);
 }
 bootstrap();

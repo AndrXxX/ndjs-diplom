c9a3d9cea4cfff64b5eb29ccb09ffcd36693ca74
Andrew Garetov <andrew1gar@yandex.ru>
2023-09-24 16:34:20 +0300

    #1 Правка AuthModule

--- reservation_service/app/src/modules/auth/auth.module.ts
+++ reservation_service/app/src/modules/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from "../users/users.module";
 import { AuthService } from "./auth.service";
 import { GenerateHashService } from "./generate-hash.service";
 
 @Module({
-  imports: [],
-  providers: [GenerateHashService],
+  imports: [UsersModule],
+  providers: [AuthService, GenerateHashService],
   exports: [AuthService, GenerateHashService],
 })
 export class AuthModule {}

bae95c6299fd2bc2bac9d0ed2038b33a6d9d0bfa
Andrew Garetov <andrew1gar@yandex.ru>
2023-09-24 23:50:22 +0300

    #1 Доработка NotAuthenticatedGuard

--- reservation_service/app/src/modules/auth/guards/not-authenticated.guard.ts
+++ reservation_service/app/src/modules/auth/guards/not-authenticated.guard.ts
@@ -1,9 +1,12 @@
-import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common"
+import { BadRequestException, CanActivate, ExecutionContext, Injectable } from "@nestjs/common"
 
 @Injectable()
 export class NotAuthenticatedGuard implements CanActivate {
   async canActivate(context: ExecutionContext) {
-    const request = context.switchToHttp().getRequest()
-    return !request.isAuthenticated()
+    const request = context.switchToHttp().getRequest();
+    if (request.isAuthenticated()) {
+      throw new BadRequestException('Route is available only for unauthorized users');
+    }
+    return true;
   }
 }

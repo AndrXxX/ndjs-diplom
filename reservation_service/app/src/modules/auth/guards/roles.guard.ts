f16b2ea4b431b154df28214cda3a5523568a03c3
Andrew Garetov <andrew1gar@yandex.ru>
2023-09-25 00:56:58 +0300

    #1 Корректная работа RolesGuard

--- reservation_service/app/src/modules/auth/guards/roles.guard.ts
+++ reservation_service/app/src/modules/auth/guards/roles.guard.ts
@@ -1,13 +1,13 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { Roles } from '../decorators/roles.decorator';
+import { RolesKey } from '../decorators/roles.decorator';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const roles = this.reflector.get(Roles, context.getHandler());
+    const roles = this.reflector.get(RolesKey, context.getHandler());
     if (!roles) {
       return true;
     }

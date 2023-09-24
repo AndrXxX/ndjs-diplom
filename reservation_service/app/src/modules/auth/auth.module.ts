3c6aefc6818ba17e61ab1e5a2e44fc83c2020c6d
Andrew Garetov <andrew1gar@yandex.ru>
2023-09-24 23:50:42 +0300

    #1 Корректная работа авторизации

--- reservation_service/app/src/modules/auth/auth.module.ts
+++ reservation_service/app/src/modules/auth/auth.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from "@nestjs/passport";
 import { UsersModule } from "../users/users.module";
 import { AuthService } from "./auth.service";
 import { GenerateHashService } from "./generate-hash.service";
+import { SessionSerializer } from "./session.serializer";
+import { LocalStrategy } from "./strategies/local.strategy";
 
 @Module({
-  imports: [UsersModule],
-  providers: [AuthService, GenerateHashService],
+  imports: [UsersModule, PassportModule.register({ session: true })],
+  providers: [AuthService, GenerateHashService, LocalStrategy, SessionSerializer],
   exports: [AuthService, GenerateHashService],
 })
 export class AuthModule {}

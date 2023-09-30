889ac56c963942a2bb7c1489dee23171d672e794
Andrew Garetov <andrew1gar@yandex.ru>
2023-09-30 14:37:07 +0300

    #1 Добавлен Endpoint DELETE /api/client/reservations/:id

--- reservation_service/app/src/modules/reservations/reservations-client.controller.ts
+++ reservation_service/app/src/modules/reservations/reservations-client.controller.ts
@@ -1,5 +1,17 @@
-import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Delete,
+  ForbiddenException,
+  Get,
+  Param,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { UserRoleEnum } from "src/enums/user-role.enum";
+import { ID } from "src/types/ID";
 import { Roles } from "../auth/decorators/roles.decorator";
 import { AuthenticatedGuard } from "../auth/guards/authenticated.guard";
 import { RolesGuard } from "../auth/guards/roles.guard";
@@ -36,5 +48,18 @@ export class ReservationsClientController {
   async reservationsList(@Request() req: any) {
     const items = await this.reservationsService.getReservations({ userId: req.user.id });
     return items.map(item => this.reservationsFormatter.format(item));
+  }
+
+  @Roles(UserRoleEnum.client)
+  @Delete('/:id')
+  async deleteReservation(@Param('id') id: ID, @Request() req: any) {
+    const item = await this.reservationsService.findById(id);
+    if (!item) {
+      throw new BadRequestException('Reservation is not exist');
+    }
+    if (item.userId !== req.user.id) {
+      throw new ForbiddenException('You can not delete reservation');
+    }
+    return await this.reservationsService.removeReservation(id);
   }
 }

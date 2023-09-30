895d713138172c9bce7ab4d2010cea4a6ab36bd0
Andrew Garetov <andrew1gar@yandex.ru>
2023-09-30 15:03:13 +0300

    #1 Удаление неиспользуемого импорта

--- reservation_service/app/src/modules/hotels/hotels-room.service.ts
+++ reservation_service/app/src/modules/hotels/hotels-room.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { ID } from "src/types/ID";
-import { UpdateHotelRoomDto } from "./dto/update-hotel-room.dto";
 import { IHotelsRoomService } from "./interfaces/hotels-room-service.interface";
 import { SearchRoomsParams } from "./interfaces/search-rooms-params.interface";
 import { HotelRoom, HotelRoomDocument } from "./mongo.schemas/hotel-room.schema";

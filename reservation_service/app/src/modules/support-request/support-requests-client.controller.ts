d36756324394a577e95f66b404e5e5d78e9fada5
Andrew Garetov <andrew1gar@yandex.ru>
2023-09-30 15:23:23 +0300

    #1 Добавлен Endpoint GET /api/client/support-requests/

--- reservation_service/app/src/modules/support-request/support-requests-client.controller.ts
+++ reservation_service/app/src/modules/support-request/support-requests-client.controller.ts
@@ -1,5 +1,7 @@
-import { Body, Controller, Post, Request, UseGuards, } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, Request, UseGuards, } from '@nestjs/common';
 import { UserRoleEnum } from "src/enums/user-role.enum";
+import { GetChatListParams } from "src/modules/support-request/interfaces/get-chat-list-params.interface";
+import { SupportRequestService } from "src/modules/support-request/support-request.service";
 import { DtoValidationPipe } from "src/validators/dto.validation.pipe";
 import { Roles } from "../auth/decorators/roles.decorator";
 import { AuthenticatedGuard } from "../auth/guards/authenticated.guard";
@@ -14,6 +16,7 @@ export class SupportRequestsClientController {
   constructor(
     private supportRequestClientService: SupportRequestClientService,
     private supportRequestFormatter: SupportRequestFormatter,
+    private supportRequestService: SupportRequestService,
   ) {}
 
   @Roles(UserRoleEnum.client)
@@ -22,5 +25,14 @@ export class SupportRequestsClientController {
     dto.user = req.user.id;
     const item = await this.supportRequestClientService.createSupportRequest(dto);
     return this.supportRequestFormatter.format(item, (await this.supportRequestClientService.getUnreadCount(item.id)).length);
+  }
+
+  @Roles(UserRoleEnum.client)
+  @Get("/")
+  async supportRequestsList(@Request() req: any, @Query() query: GetChatListParams) {
+    query.user = req.user.id;
+    const items = await this.supportRequestService.findSupportRequests(query);
+    const counter = this.supportRequestClientService.getUnreadCount;
+    return Promise.all(items.map(async item => this.supportRequestFormatter.format(item, (await counter(item.id)).length)));
   }
 }

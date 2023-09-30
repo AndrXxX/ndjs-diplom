fd976c8a11d67dd2aaeb61fddae759627b2390ae
Andrew Garetov <andrew1gar@yandex.ru>
2023-09-30 12:42:56 +0300

    #1 Добавлен Endpoint PUT /api/admin/hotels/:id

--- reservation_service/app/src/modules/hotels/hotels-admin.controller.ts
+++ reservation_service/app/src/modules/hotels/hotels-admin.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, Query, UseGuards } from '@nestjs/common';
 import { UserRoleEnum } from "src/enums/user-role.enum";
 import { Roles } from "src/modules/auth/decorators/roles.decorator";
 import { AuthenticatedGuard } from "src/modules/auth/guards/authenticated.guard";
 import { RolesGuard } from "src/modules/auth/guards/roles.guard";
 import { HotelsFormatter } from "src/modules/hotels/hotels.formatter";
+import { ID } from "src/types/ID";
 import { CreateHotelDto } from "./dto/create-hotel.dto";
 import { HotelsService } from "./hotels.service";
 import { SearchHotelParams } from "./interfaces/search-hotel-params.interface";
@@ -19,7 +20,7 @@ export class HotelsAdminController {
   @Roles(UserRoleEnum.admin)
   @Post('/hotels/')
   async addHotel(@Body() createHotelDto: CreateHotelDto) {
-    return this.hotelsFormatter.format(await this.hotelsService.create(createHotelDto))
+    return this.hotelsFormatter.format(await this.hotelsService.create(createHotelDto));
   }
 
   @Roles(UserRoleEnum.admin)
@@ -27,5 +28,11 @@ export class HotelsAdminController {
   async hotelsList(@Query() query: SearchHotelParams) {
     const hotels = await this.hotelsService.search(query);
     return hotels.map(hotel => this.hotelsFormatter.format(hotel));
+  }
+
+  @Roles(UserRoleEnum.admin)
+  @Put('/hotels/:id')
+  async updateHotel(@Param('id') id: ID, @Body() updateHotelDto: UpdateHotelParams) {
+    return this.hotelsFormatter.format(await this.hotelsService.update(id, updateHotelDto));
   }
 }

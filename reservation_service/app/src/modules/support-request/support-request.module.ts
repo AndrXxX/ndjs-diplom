6fab0ac75e4f7414d804d713692ec90a42cb7c13
Andrew Garetov <andrew1gar@yandex.ru>
2023-09-30 15:30:45 +0300

    #1 Добавлен метод formatForManager

--- reservation_service/app/src/modules/support-request/support-request.module.ts
+++ reservation_service/app/src/modules/support-request/support-request.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from "@nestjs/mongoose";
+import { UsersModule } from "src/modules/users/users.module";
 import { Message, MessageSchema } from "./mongo.schemas/message.schema";
 import { SupportRequest, SupportRequestSchema } from "./mongo.schemas/support-request.schema";
 import { SupportRequestFormatter } from "./support-request.formatter";
@@ -12,6 +13,7 @@ import { SupportRequestsClientController } from "./support-requests-client.contr
       { name: Message.name, schema: MessageSchema },
       { name: SupportRequest.name, schema: SupportRequestSchema },
     ]),
+    UsersModule,
   ],
   providers: [SupportRequestService, SupportRequestFormatter],
   exports: [SupportRequestService, SupportRequestFormatter],

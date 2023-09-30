4ed94d83de9f32c6dc0303b20537366e7754ee5d
Andrew Garetov <andrew1gar@yandex.ru>
2023-09-30 13:00:46 +0300

    #1 Добавлен Endpoint PUT /api/admin/hotel-rooms/:id

--- reservation_service/app/src/modules/hotels/hotel-rooms-admin.controller.ts
+++ reservation_service/app/src/modules/hotels/hotel-rooms-admin.controller.ts
@@ -1,9 +1,11 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { UserRoleEnum } from "src/enums/user-role.enum";
 import { Roles } from "src/modules/auth/decorators/roles.decorator";
 import { AuthenticatedGuard } from "src/modules/auth/guards/authenticated.guard";
 import { RolesGuard } from "src/modules/auth/guards/roles.guard";
+import { ID } from "src/types/ID";
 import { CreateHotelRoomDto } from "./dto/create-hotel-room.dto";
+import { UpdateHotelRoomDto } from "./dto/update-hotel-room.dto";
 import { HotelsRoomFormatter } from "./hotels-room.formatter";
 import { HotelsRoomService } from "./hotels-room.service";
 
@@ -20,5 +22,14 @@ export class HotelRoomsAdminController {
   async addHotelRoom(@Body() createHotelRoomDto: CreateHotelRoomDto) {
     // TODO: Этот запрос предполагает загрузку файлов и должен использовать формат multipart/form-data.
     return this.hotelsRoomFormatter.format(await this.hotelsRoomService.create(createHotelRoomDto));
+  }
+
+  @Roles(UserRoleEnum.admin)
+  @Put('/:id')
+  async updateHotelRoom(@Param('id') id: ID, @Body() updateHotelRoomDto: UpdateHotelRoomDto) {
+    // TODO: Этот запрос предполагает загрузку файлов и должен использовать формат multipart/form-data.
+    // TODO: При обновлении может быть отправлен одновременно список ссылок на уже загруженные картинки и список файлов с новыми картинками.
+    // TODO: При использовании multer список загруженных файлов можно получить через @UploadedFiles(). Этот список нужно объединить со списком, который пришёл в body.
+    return this.hotelsRoomFormatter.format(await this.hotelsRoomService.update(id, updateHotelRoomDto));
   }
 }

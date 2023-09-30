3b1a5a4a8c64401ea87c4699bc46d3b78e23d9d0
Andrew Garetov <andrew1gar@yandex.ru>
2023-09-30 12:57:09 +0300

    #1 Правка CreateHotelRoomDto

--- reservation_service/app/src/modules/hotels/dto/create-hotel-room.dto.ts
+++ reservation_service/app/src/modules/hotels/dto/create-hotel-room.dto.ts
@@ -1,23 +1,13 @@
-import { IsArray, IsBoolean, IsDate, IsDefined, IsString } from "class-validator";
+import { IsArray, IsDefined, IsString } from "class-validator";
 import { ObjectId } from "mongoose";
-import { HotelRoom } from "src/modules/hotels/mongo.schemas/hotel-room.schema";
 
-export class CreateHotelRoomDto extends HotelRoom {
+export class CreateHotelRoomDto {
   @IsDefined()
-  hotel: ObjectId;
+  hotelId: ObjectId;
 
   @IsString()
   description: string;
 
   @IsArray()
-  images: string[];
-
-  @IsDefined() @IsDate()
-  createdAt: Date;
-
-  @IsDefined() @IsDate()
-  updatedAt: Date;
-
-  @IsDefined() @IsBoolean()
-  isEnabled: boolean;
+  images: File[];
 }

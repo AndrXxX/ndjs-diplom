5e80af28e5c79dc36959d446d0320b95520c42b8
Andrew Garetov <andrew1gar@yandex.ru>
2023-10-03 17:47:53 +0300

    #1 Добавлен sessionMiddleware

--- reservation_service/app/src/main.ts
+++ reservation_service/app/src/main.ts
@@ -1,21 +1,15 @@
 import { NestFactory } from '@nestjs/core';
-import * as session from "express-session"
 import * as passport from "passport"
 import { AppModule } from './app.module';
 import config from './config';
 import { UnifiedExceptionFilter } from "./filters/unified.exception.filter";
+import sessionMiddleware from "./middleware/session";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new UnifiedExceptionFilter());
 
-  app.use(
-    session({
-      secret: config.cookieSecret,
-      resave: false,
-      saveUninitialized: false,
-    })
-  )
+  app.use(sessionMiddleware)
   app.use(passport.initialize())
   app.use(passport.session())
 

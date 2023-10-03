d14b3173904851991bb0aa340fb1a138fc44063a
Andrew Garetov <andrew1gar@yandex.ru>
2023-10-03 16:58:09 +0300

    #1 Добавлен Endpoint POST /api/common/support-requests/:id/messages

--- reservation_service/app/src/modules/support-request/support-requests-common.controller.ts
+++ reservation_service/app/src/modules/support-request/support-requests-common.controller.ts
@@ -1,7 +1,19 @@
-import { Controller, ForbiddenException, Get, NotFoundException, Param, Request, UseGuards, } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  ForbiddenException,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { UserRoleEnum } from "src/enums/user-role.enum";
+import { CreateSupportRequestDto } from "src/modules/support-request/dto/send-message.dto";
 import { User } from "src/modules/users/mongo.schemas/user.schema";
 import { ID } from "src/types/ID";
+import { DtoValidationPipe } from "src/validators/dto.validation.pipe";
 import { Roles } from "../auth/decorators/roles.decorator";
 import { AuthenticatedGuard } from "../auth/guards/authenticated.guard";
 import { RolesGuard } from "../auth/guards/roles.guard";
@@ -21,6 +33,20 @@ export class SupportRequestsCommonController {
   async messages(@Param('id') id: ID, @Request() req: any) {
     const supportRequest = await this.getSupportRequest(id, req.user);
     return supportRequest.messages.map(item => this.messageFormatter.format(item));
+  }
+
+  @Roles(UserRoleEnum.manager, UserRoleEnum.client)
+  @Post("/:id/messages")
+  async sendMessage(
+    @Param('id') id: ID,
+    @Request() req: any,
+    @Body(DtoValidationPipe) dto: CreateSupportRequestDto
+  ) {
+    await this.getSupportRequest(id, req.user);
+    dto.supportRequest = id;
+    dto.author = req.user.id;
+    const message = await this.supportRequestService.sendMessage(dto)
+    return this.messageFormatter.format(message);
   }
 
   private async getSupportRequest(id: ID, user: User) {

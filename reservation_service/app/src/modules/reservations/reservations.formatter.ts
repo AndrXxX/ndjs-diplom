01800f4e5dc0afddb102aa7eebfc3bfbf5d497f1
Andrew Garetov <andrew1gar@yandex.ru>
2023-10-08 15:27:14 +0300

    #1 Доработка ReservationsFormatter

--- reservation_service/app/src/modules/reservations/reservations.formatter.ts
+++ reservation_service/app/src/modules/reservations/reservations.formatter.ts
@@ -1,6 +1,9 @@
 import { Injectable } from "@nestjs/common";
+import { isObject } from "@nestjs/common/utils/shared.utils";
 import { HotelsRoomFormatter } from "src/modules/hotels/hotels-room.formatter";
 import { HotelsFormatter } from "src/modules/hotels/hotels.formatter";
+import { HotelRoom } from "src/modules/hotels/mongo.schemas/hotel-room.schema";
+import { Hotel } from "src/modules/hotels/mongo.schemas/hotel.schema";
 import { Reservation } from "src/modules/reservations/mongo.schemas/reservation.schema";
 
 @Injectable()
@@ -13,11 +16,13 @@ export class ReservationsFormatter {
 
     public format(reservation: Reservation) {
         const { dateStart, dateEnd } = reservation;
+        const room: any | HotelRoom = isObject(reservation.roomId) ? reservation.roomId : {};
+        const hotel: any | Hotel = isObject(room?.hotel) ? room.hotel : {};
         return {
             startDate: dateStart,
             endDate: dateEnd,
-            hotelRoom: this.hotelsRoomFormatter.formatForClient(reservation.roomId as any), // TODO
-            hotel: this.hotelsFormatter.formatForClient(reservation.hotelId as any), // TODO
+            hotelRoom: this.hotelsRoomFormatter.formatForClient(room),
+            hotel: this.hotelsFormatter.formatForClient(hotel),
         };
     }
 }

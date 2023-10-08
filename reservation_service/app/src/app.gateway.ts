f0f81e963864a24d22cc218818ff51f8bc0faf08
Andrew Garetov <andrew1gar@yandex.ru>
2023-10-08 13:01:52 +0300

    #1 Подключение авторизации для AppGateway

--- reservation_service/app/src/app.gateway.ts
+++ reservation_service/app/src/app.gateway.ts
@@ -1,12 +1,17 @@
-import { BadRequestException, UseFilters } from "@nestjs/common";
+import { BadRequestException, UseFilters, UseGuards } from "@nestjs/common";
 import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import { UserRoleEnum } from "./enums/user-role.enum";
 import { WsExceptionFilter } from "./filters/ws.exception.filter";
+import { Roles } from "./modules/auth/decorators/roles.decorator";
+import { AuthenticatedGuard } from "./modules/auth/guards/authenticated.guard";
+import { RolesGuard } from "./modules/auth/guards/roles.guard";
 import { SupportRequestMessageFormatter } from "./modules/support-request/support-request-message.formatter";
 import { SupportRequestService } from "./modules/support-request/support-request.service";
 import { ID } from "./types/ID";
 
 @UseFilters(new WsExceptionFilter)
+@UseGuards(AuthenticatedGuard, RolesGuard)
 @WebSocketGateway({ cors: true })
 export class AppGateway {
   constructor(
@@ -17,6 +22,7 @@ export class AppGateway {
   @WebSocketServer()
   server: Server;
 
+  @Roles(UserRoleEnum.manager, UserRoleEnum.client)
   @SubscribeMessage('subscribeToChat')
   async subscribeToChat(
     @MessageBody("payload") dto: { payload: { chatId: ID} },

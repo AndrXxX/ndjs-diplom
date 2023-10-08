4e55dfc2c2c769887bbd06f6d04b632094f39a52
Andrew Garetov <andrew1gar@yandex.ru>
2023-10-08 15:26:20 +0300

    #1 Доработка проверки дат в CreateReservationDto

--- reservation_service/app/src/modules/reservations/dto/create-reservation.dto.ts
+++ reservation_service/app/src/modules/reservations/dto/create-reservation.dto.ts
@@ -1,13 +1,13 @@
-import { IsDate, IsDefined, IsString } from "class-validator";
+import { IsDateString, IsDefined, IsString } from "class-validator";
 import { ID } from "src/types/ID";
 
 export class CreateReservationDto {
   @IsDefined() @IsString()
   hotelRoom: ID;
 
-  @IsDefined() @IsDate()
+  @IsDefined() @IsDateString()
   startDate: Date;
 
-  @IsDefined() @IsDate()
+  @IsDefined() @IsDateString()
   endDate: Date;
 }

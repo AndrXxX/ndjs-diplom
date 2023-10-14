024a3fa067d40a4154b24467b683825b75021ba8
Andrew Garetov <andrew1gar@yandex.ru>
2023-10-14 20:31:44 +0300

    #1 Доработка WsAuthenticatedGuard

--- reservation_service/app/src/modules/auth/guards/ws.authenticated.guard.ts
+++ reservation_service/app/src/modules/auth/guards/ws.authenticated.guard.ts
@@ -1,9 +1,13 @@
 import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common"
+import { WsException } from "@nestjs/websockets";
 
 @Injectable()
 export class WsAuthenticatedGuard implements CanActivate {
   async canActivate(context: ExecutionContext) {
     const client = context.switchToWs().getClient();
-    return client.request?.isAuthenticated();
+    if (!client.request?.isAuthenticated()) {
+      throw new WsException('Unauthorized user');
+    }
+    return true;
   }
 }

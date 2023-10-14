5e807675be5b016c47c600f29cebac1e6d79615e
Andrew Garetov <andrew1gar@yandex.ru>
2023-10-14 20:07:33 +0300

    #1 Добавлен WsSessionAdapter

--- reservation_service/app/src/main.ts
+++ reservation_service/app/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import * as passport from "passport"
+import { WsSessionAdapter } from "./adapters/ws.session.adapter";
 import { AppModule } from './app.module';
 import config from './config';
 import { UnifiedExceptionFilter } from "./filters/unified.exception.filter";
@@ -12,6 +13,8 @@ async function bootstrap() {
   app.use(sessionMiddleware)
   app.use(passport.initialize())
   app.use(passport.session())
+
+  app.useWebSocketAdapter(new WsSessionAdapter(sessionMiddleware, app));
 
   await app.listen(config.port);
 }

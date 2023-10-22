c87731a25f3876964f5ea44dcc9b188f829c9c1e
Andrew Garetov <andrew1gar@yandex.ru>
2023-10-22 13:03:23 +0300

    #1 Доработка оформления в app/src/adapters

--- reservation_service/app/src/adapters/ws.session.adapter.ts
+++ reservation_service/app/src/adapters/ws.session.adapter.ts
@@ -1,7 +1,7 @@
 import { INestApplicationContext } from "@nestjs/common";
 import { IoAdapter } from "@nestjs/platform-socket.io";
 import express from "express";
-import * as passport from "passport"
+import * as passport from "passport";
 import { Server, ServerOptions } from "socket.io";
 
 export class WsSessionAdapter extends IoAdapter {
@@ -15,7 +15,8 @@ export class WsSessionAdapter extends IoAdapter {
   create(port: number, options?: ServerOptions): Server {
     const server: Server = super.create(port, options);
 
-    const wrap = (middleware: any) => (socket: any, next: any) => middleware(socket.request, {}, next);
+    const wrap = (middleware: any) => (socket: any, next: any) =>
+      middleware(socket.request, {}, next);
     server.use(wrap(this.session));
     server.use(wrap(passport.initialize()));
     server.use(wrap(passport.session()));
